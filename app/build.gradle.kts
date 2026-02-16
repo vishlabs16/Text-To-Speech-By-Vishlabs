1f5539534d7703094e24ac2d2d71cb182f78a950
Vishlabs <vishlabs16@gmail.com>
2026-02-16 08:28:04 +0530

    App Live 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rach.texttospeechbyvishlabs"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
